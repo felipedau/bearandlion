303f25f06c9859ec7de4fda1d72ef475c0702d53
Felipe Dau <dau.felipe@gmail.com>
2016-06-05 04:16:32 +0000

    Create hash vector tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,8 @@
 import pytest
 
-from bearandlion.utils import cipher, xor
+from bearandlion.utils import cipher, hash, xor
+
+from .vectors.hashes.sha256.sha256_vector_sets import Sha256VectorSets
 
 
 AES_ACCEPTED_KEY_LENGTHS = [16, 24, 32]
@@ -20,6 +22,15 @@ def test_aes_counter_length_based_on_key(key_length):
     plaintext = b'\x01' * key_length
     ciphertext = cipher.encrypt(key, plaintext)
     cipher.decrypt(key, ciphertext)
+
+
+SHA256_VECTOR_SETS = Sha256VectorSets.parse_vector_sets()
+
+
+@pytest.mark.parametrize('vector_set', SHA256_VECTOR_SETS.keys())
+def test_sha256_vector_set(vector_set):
+    for vector in SHA256_VECTOR_SETS[vector_set]:
+        assert hash(vector.message) == vector.digest
 
 
 DATA_LENGTHS = range(3)
